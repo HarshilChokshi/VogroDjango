6bd969948437110cf34ddb5bf9d7b60d99510f2a
Harshil Chokshi <Harsh@Harshils-MacBook-Pro.local>
2020-05-13 19:36:30 -0400

    Added gunicorna dn nginx to run django in prod mode.

--- VogroDjango/settings.py
+++ VogroDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r&%iux1733f4j-=$t!yavnnn+%)*)0$%^yl*(cz*&)w#5=6b+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 CRONJOBS = [
     ('00 04 * * *', 'vogro_api.cron.moveAllExpiredTasksToUnMatchedTaskTable')
--- configuration/setup_steps.sh
+++ configuration/setup_steps.sh
@@ -13,6 +13,8 @@ pip3 install "djangorestframework==3.11.0"
 sudo apt install mysql-server
 sudo apt install libmysqlclient-dev default-libmysqlclient-dev
 pip3 install "mysqlclient==1.4.6"
+pip3 install "gunicorn==20.0.4"
+sudo apt-get install nginx
 
 # Setup database and db user
 sudo mysql -u root
@@ -22,6 +24,13 @@ grant usage on *.* to 'django'@'localhost';
 grant all privileges on vogro_api.* to 'django'@'localhost';
 FLUSH PRIVILEGES;
 
+#Setup nginx
+sudo mv ~/VogroDjango/configuration/djtrump /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/djtrump /etc/nginx/sites-enabled
+#Command to check if setup is good so far
+sudo nginx -t
+
+
 # Copy over code, start db, and django server
 scp -r ~/VogroDjango/ ubuntu@ec2-13-59-38-83.us-east-2.compute.amazonaws.com:/home/ubuntu
 sudo touch /etc/mysql/my.cnf
@@ -30,5 +39,6 @@ sudo systemctl daemon-reload
 sudo systemctl restart mysql
 python3 manage.py migrate
 python3 manage.py loaddata /tmp/datadump.json
-nohup python3 manage.py runserver ec2-13-59-38-83.us-east-2.compute.amazonaws.com:8000 > /tmp/django-output.log &
+gunicorn --daemon --bind unix:/home/ubuntu/VogroDjango/VogroDjango.sock VogroDjango.wsgi > /tmp/django-output.log
+sudo service nginx restart
 python3 manage.py crontab add
